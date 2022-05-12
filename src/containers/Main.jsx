eacb23076721a5940f309ae3af039b3fad67e675
jonny0702 <jonathanvergararuiz@gmail.com>
2022-05-11 19:37:28 -0500

    ♻️REFACTOR: Refactoring the dependecies

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -20,67 +20,75 @@ function Main() {
   const API_KEY = process.env.API_KEY;
   const API_WEATHER = process.env.API_WEATHER;
   const API_FORECAST = process.env.API_FORECAST;
-  
+
   const [dataWeather, setDataWeather] = useState({});
   const [dataForecast, setDataForecast] = useState([]);
   const [localAddress, setLocalAddress] = useState('');
-  const [lat, setLat] = useState(0)
-  const [lng, setLng] = useState(0)
-  
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
 
   const hasWeather = Object.values(dataWeather).length > 0;
 
-
-const savePositionToState = (position) =>{
-  setLat(position.coords.latitude)
-  setLng(position.coords.longitude)
-
-}
-//Api Actual Weather
-const getWeatherData = async ()=>{
+  const savePositionToState = (position) => {
+    setLat(position.coords.latitude);
+    setLng(position.coords.longitude);
+  };
+  //Api Actual Weather
+  const getWeatherData = async () => {
     try {
-      await window.navigator.geolocation.getCurrentPosition(savePositionToState)
-      const responseWeather = await fetch(`${API_WEATHER}?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`);
+      await window.navigator.geolocation.getCurrentPosition(
+        savePositionToState
+      );
+      const responseWeather = await fetch(
+        `${API_WEATHER}?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`
+      );
       const data = await responseWeather.json();
       setDataWeather(data);
       setLocalAddress(data.name);
-      } catch (error) {
-        console.error('API ERROR', error);
-      }
-}
-// Api Forcast
-const getForecastData = async()=>{
-      try {
-        await window.navigator.geolocation.getCurrentPosition(savePositionToState)
-        const responseForecast = await fetch(`${API_FORECAST}?lat=${lat}&lon=${lng}&exclude=current,minutely,hourly,alerts&units=metric&appid=${API_KEY}`);
-        const forecast = await responseForecast.json();
-        setDataForecast(forecast.daily);
-      } catch (error) {
-        console.error(error);
-      }
-}
-  useEffect(() => { 
-    getWeatherData()
-    getForecastData()
-    return {dataWeather, dataForecast}
-  },[lat,lng]);
-  return (
-    <div className="App-container" >
-    {
-      hasWeather &&(
-        <div className='img-background'>
-          <img src={`https://source.unsplash.com/1920x1080/?${dataWeather.weather[0].main}`} className='background_photo'/>
-        </div>
-      )
+    } catch (error) {
+      console.error('API ERROR', error);
     }
-      <Header isOpen={open} address={localAddress}/>
+  };
+  // Api Forcast
+  const getForecastData = async () => {
+    try {
+      await window.navigator.geolocation.getCurrentPosition(
+        savePositionToState
+      );
+      const responseForecast = await fetch(
+        `${API_FORECAST}?lat=${lat}&lon=${lng}&exclude=current,minutely,hourly,alerts&units=metric&appid=${API_KEY}`
+      );
+      const forecast = await responseForecast.json();
+      setDataForecast(forecast.daily);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getWeatherData();
+    getForecastData();
+    return { dataWeather, dataForecast };
+  }, [lat, lng]);
+  return (
+    <div className="App-container">
+      {hasWeather && (
+        <div className="img-background">
+          <img
+            src={`https://source.unsplash.com/1920x1080/?${dataWeather.weather[0].main}`}
+            className="background_photo"
+          />
+        </div>
+      )}
+      <Header isOpen={open} address={localAddress} />
       {
-//data Components
+        //data Components
         <WeatherState>
           {hasWeather && (
             <>
               <h1 className="weather-temperature">{dataWeather.main.temp}ºC</h1>
-              <span className="weather-status">{dataWeather.weather[0].description}</span>
+              <span className="weather-status">
+                {dataWeather.weather[0].description}
+              </span>
               <div className="weather-ica">
                 <Button
                   variant="outlined"
@@ -102,10 +110,7 @@ const getForecastData = async()=>{
         </WeatherState>
       }
       <Forecast forecastData={dataForecast} />
-      <Meteorology
-        weatherData={dataWeather}
-        uviData={dataForecast}
-      />
+      <Meteorology weatherData={dataWeather} uviData={dataForecast} />
       <div className="footer">
         <p>Los datos Proporcionados son parte de: OpenWeather</p>
       </div>
